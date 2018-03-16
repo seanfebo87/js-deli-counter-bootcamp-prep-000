93c6914651da17f65e944efb1a2e107f258e81cd
Sean Febo <seanfebo@techie.com>
2018-03-16 22:20:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,5 @@ function nowServing(katzDeliLine) {
   if(katzDeliLine.length <= 0) {
     return ("There is nobody waiting to be served!")
 } 
-  katzDeliLine.shift()
   return ("Currently serving " + katzDeliLine[0] + ".")
 }
