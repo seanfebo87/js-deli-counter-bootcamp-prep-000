32dff1c9d0ab71fdb3d2cea696a107d0393e190c
Sean Febo <seanfebo@techie.com>
2018-03-16 22:08:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(katzDeliLine, n) {
   return ("Welcome, " + (n) + "." + " You are number " + katzDeliLine.length + " in line.")
 }
 
-function nowServing() {
+function nowServing(katzDeliLine) {
   if(katzDeliLine.length <= 0) {
     return ("There is nobody waiting to be served")
 } 
