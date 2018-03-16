5d09c108ab022cf17dd509e283af1a43b1bb628f
Sean Febo <seanfebo@techie.com>
2018-03-16 22:06:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function takeANumber(katzDeliLine, n) {
 }
 
 function nowServing() {
-  if
+  if(katzDeliLine.length <= 0) {
+    return ("There is nobody waiting to be served")
+} 
   katzDeliLine.shift()
   return katzDeliLine[0]
 }
